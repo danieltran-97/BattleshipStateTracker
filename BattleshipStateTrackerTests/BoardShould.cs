5933cb3f4444576532b91c21c287ea159bdaf0ab
danieltran-97 <danielmctran@gmail.com>
2021-06-24 00:07:11 +1000

    commit

---
+++ BattleshipStateTrackerTests/BoardShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace BattleshipStateTrackerTests
+{
+    public class BoardShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
